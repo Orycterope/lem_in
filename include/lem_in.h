774161e52d24f2e0712c500da676f46caf9f4752
Thomas VERMEILH <tvermeil@student.42.fr>
2016-01-28 23:53:46 +0100

    few fixes

--- include/lem_in.h
+++ include/lem_in.h
@@ -6,7 +6,7 @@
 /*   By: tvermeil <tvermeil@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/01/28 19:43:28 by tvermeil          #+#    #+#             */
-/*   Updated: 2016/01/28 22:57:22 by tvermeil         ###   ########.fr       */
+/*   Updated: 2016/01/28 23:51:02 by tvermeil         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,12 +14,13 @@
 # define LEM_IN_H
 # include "get_next_line.h"
 # include "libft.h"
-# include "path.h"
 # include "room.h"
+# include "path.h"
 # include "tunnel.h"
+# include "ant.h"
 
 void	error(void);
 int		get_lem_nbr(void);
-t_room	*save_rooms(void)
+t_room	*save_rooms(void);
 
 #endif
--- src/lem_in.c
+++ src/lem_in.c
@@ -6,13 +6,15 @@
 /*   By: tvermeil <tvermeil@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/01/28 14:42:04 by tvermeil          #+#    #+#             */
-/*   Updated: 2016/01/28 22:52:41 by tvermeil         ###   ########.fr       */
+/*   Updated: 2016/01/28 23:53:06 by tvermeil         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "lem_in.h"
+
 void	error(void)
 {
-	ft_putstr_fd("ERROR\n");
+	ft_putstr_fd("ERROR\n", 2);
 	exit(0);
 }
 
@@ -28,14 +30,14 @@ void	execute_turn(t_room *rooms, int *first_ant, int last_ant, t_path *shortest)
 	ant = *first_ant;
 	while (ant <= last_ant)
 	{
-		room = get_ant_position(ant);
+		room = get_ant_position(ant, rooms);
 		if (room == NULL)
 			continue;
 		moved = ant_decide(room, ant - last_ant, rooms, shortest);
 		if (room == start)
 		{
 			if (moved == 1)
-				*first_ant++;
+				*first_ant += 1;
 			else
 				break;
 		}
@@ -54,8 +56,8 @@ void	lem_in(t_room *rooms, int ants, t_room *start, t_room *end)
 	while (shortest == NULL && i < 100)
 		shortest = get_shortest_path(start, end, i++, 0);
 	if (shortest == NULL)
-		error;
-	while (start->ant < ants || get_ant_position(ants) != NULL)
+		error();
+	while (start->ant < ants || get_ant_position(ants, rooms) != NULL)
 	{
 		execute_turn(rooms, &first_ant, ants, shortest);
 		ft_putchar('\n');
